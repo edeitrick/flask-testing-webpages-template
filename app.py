45b9cf03c839c86ab5e42fa6b32de1959ab5ffb4
Elise Deitrick <edeitrick@codio.com>
2023-07-04 23:34:39 +0000

    adding webhook code

--- app.py
+++ app.py
@@ -1,10 +1,21 @@
-from flask import Flask, render_template, url_for, flash, redirect
+from flask import Flask, render_template, url_for, flash, redirect, request
 from forms import RegistrationForm
 from flask_behind_proxy import FlaskBehindProxy
+import git
 
 app = Flask(__name__)
 proxied = FlaskBehindProxy(app)
 app.config['SECRET_KEY'] = 'c29bcfa698752666def85f68880d22d8'
+
+@app.route('/update_server', methods=['POST'])
+    def webhook():
+        if request.method == 'POST':
+            repo = git.Repo('path/to/git_repo')
+            origin = repo.remotes.origin
+            origin.pull()
+            return 'Updated PythonAnywhere successfully', 200
+        else:
+            return 'Wrong event type', 400
 
 @app.route("/")
 @app.route("/home")
